f2ea9d4053bf6a5e92a3dd45db43ffdfec462192
kevin <kthurman59@gmail.com>
2024-04-02 14:58:26 -0500

    spin loader

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -23,12 +23,28 @@ async function getUserData() {
   }),
  ])
 
- 
+ return {
+  userCount,
+  averageValuePerUser: userCount === 0 ? 0 : (orderData._sum.pricePaidInCents || 0) / userCount / 100
+ }
+}
 
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    db.product.count({ where: { isAvailableForPurchase: true }}),
+    db.product.count({ where: { isAvailableForPurchase: false }})
+  ])
+
+  return { activeCount, inactiveCount }
 }
 
 export default async function AdminDashboard() {
-  const salesData = await getSalesData()
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData()
+  ])
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <DashboardCard
@@ -38,8 +54,13 @@ export default async function AdminDashboard() {
       />
       <DashboardCard
         title="Customers"
-        subtitle={`${formatCurrency()} Average Value`}
-        body={formatCurrency()}
+        subtitle={`${formatCurrency(userData.averageValuePerUser)} Average Value`}
+        body={formatCurrency(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+        body={formatCurrency(productData.activeCount)}
       />
     </div>
   )
