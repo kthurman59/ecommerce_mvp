18dd5c5544665bc78df5d502f420e0f9aa1e4a15
kevin <kthurman59@gmail.com>
2024-04-02 13:12:57 -0500

    layout update

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,34 @@
-import { Card, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+
+function getSalesData() {
+  db
+}
 
 export default function AdminDashboard() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <Card>
-        <CardHeader></CardHeader>
-      </Card>
+      <DashboardCard title="Sales" subtitle="Test" body="body" />
     </div>
   )
 }
+
+type DashboardCardProps = {
+  title: string
+  subtitle: string
+  body: string
+}
+
+
+function DashboardCard({title, subtitle, body}:
+  DashboardCardProps) {
+    return       <Card>
+    <CardHeader>
+      <CardTitle>{title}</CardTitle>
+      <CardDescription>{subtitle}</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <p>{body}</p>
+    </CardContent>
+  </Card>
+  }
