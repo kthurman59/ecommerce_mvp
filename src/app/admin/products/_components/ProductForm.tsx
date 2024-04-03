815c325826c41d9b9bc112805b3afaaea2655c2b
kevin <kthurman59@gmail.com>
2024-04-03 11:31:34 -0500

    product page build

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,5 +1,17 @@
 "use client"
 
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 export function ProductForm() {
-  return
+  return <form className="space-y-8">
+    <div className="space-y-2">
+      <Label htmlFor="name">Name</Label>
+      <Input type="text" id="name" name="name" required />
+    </div>
+    <div className="space-y-2">
+      <Label htmlFor="priceInCents">Price In Cents</Label>
+      <Input type="text" id="priceInCents" name="priceInCents" required value={priceInCents} onChange={e => setPriceInCents(e.target.value)} />
+    </div>
+  </form>
 }
