6c27e6f736250e72a33c7284a1bd4acbe09c62d6
kevin <kthurman59@gmail.com>
2024-04-03 10:58:42 -0500

    UI build out

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/PageHeader";
+import Link from "next/link";
 
 export default function AdminProductsPage() {
-  return (<PageHeader>
-    Products
-  </PageHeader>
+  return (
+  <div className="flex justify-between items-center gap-4">
+    <PageHeader>Products</PageHeader>
+    <Button asChild>
+      <Link href="/admin/products/new">Add Product</Link>
+    </Button>
+  </div>
   )
 }
