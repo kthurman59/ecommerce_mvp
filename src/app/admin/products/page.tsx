2680c5e46cd0f4ab75e78f1445d34c36d8105e68
kevin <kthurman59@gmail.com>
2024-04-04 07:58:36 -0500

    admin work

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/PageHeader";
 import Link from "next/link";
 import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import db from "@/db/db";
 
 export default function AdminProductsPage() {
   return (
@@ -17,21 +18,32 @@ export default function AdminProductsPage() {
  )
 }
 
-function ProductsTable() {
-  return <Table>
-    <TableHeader>
-      <TableRow>
-        <TableHead className="w-0">
-          <span className="sr-only">Available for Purchase</span>
-        </TableHead>
-        <TableHead>Name</TableHead>
-        <TableHead>Price</TableHead>
-        <TableHead>Orders</TableHead>
-        <TableHead className="w-0">
-          <span className="sr-only">Actions</span>
-        </TableHead>
-      </TableRow>
-    </TableHeader>
-    <TableBody> </TableBody>
-  </Table>
+async function ProductsTable() {
+  const products = await db.product.findMany({ 
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      _count: { select: { orders: true }}
+    },
+  })
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available for Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody> </TableBody>
+    </Table>
+  )
 }
