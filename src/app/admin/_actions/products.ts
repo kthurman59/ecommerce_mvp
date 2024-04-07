81cc8f33587f9d030e9c923072b3eb43c415d142
kevin <kthurman59@gmail.com>
2024-04-07 14:07:02 -0500

    fixes

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -6,17 +6,18 @@ import fs from "fs/promises"
 import { notFound, redirect } from "next/navigation"
 import { revalidatePath } from "next/cache"
 
-const fileSchema = z.instanceof(File, { message: "Required"})
-const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/") )
+const fileSchema = z.instanceof(File, { message: "Required" })
+const imageSchema = fileSchema.refine(
+  file => file.size === 0 || file.type.startsWith("image/")
+)
 
 const addSchema = z.object({
   name: z.string().min(1),
   description: z.string().min(1),
   priceInCents: z.coerce.number().int().min(1),
   file: fileSchema.refine(file => file.size > 0, "Required"),
-  image: imageSchema.refine(file => file.size > 0, "Required")
+  image: imageSchema.refine(file => file.size > 0, "Required"),
 })
-
 
 export async function addProduct(prevState: unknown, formData: FormData) {
   const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
@@ -32,10 +33,13 @@ export async function addProduct(prevState: unknown, formData: FormData) {
 
   await fs.mkdir("public/products", { recursive: true })
   const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
-  await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+  await fs.writeFile(
+    `public${imagePath}`,
+    Buffer.from(await data.image.arrayBuffer())
+  )
 
-  await db.product.create({ 
-    data: { 
+  await db.product.create({
+    data: {
       isAvailableForPurchase: false,
       name: data.name,
       description: data.description,
@@ -53,18 +57,21 @@ export async function addProduct(prevState: unknown, formData: FormData) {
 
 const editSchema = addSchema.extend({
   file: fileSchema.optional(),
-  image: imageSchema.optional()
+  image: imageSchema.optional(),
 })
 
-export async function updateProduct(id: string, prevState: unknown, formData: FormData) {
+export async function updateProduct(
+  id: string,
+  prevState: unknown,
+  formData: FormData
+) {
   const result = editSchema.safeParse(Object.fromEntries(formData.entries()))
   if (result.success === false) {
     return result.error.formErrors.fieldErrors
   }
 
   const data = result.data
-  const product = await db.product.findUnique({ where: {
-    id } })
+  const product = await db.product.findUnique({ where: { id } })
 
   if (product == null) return notFound()
 
@@ -73,22 +80,21 @@ export async function updateProduct(id: string, prevState: unknown, formData: Fo
     await fs.unlink(product.filePath)
     filePath = `products/${crypto.randomUUID()}-${data.file.name}`
     await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
-}
+  }
 
-let imagePath = product.imagePath
-if (data.image != null && data.image.size > 0) {
-  await fs.unlink(`public${product.imagePath}`)
-  imagePath = `products/${crypto.randomUUID()}-${data.image.name}`
-  await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
-}
+  let imagePath = product.imagePath
+  if (data.image != null && data.image.size > 0) {
+    await fs.unlink(`public${product.imagePath}`)
+    imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+    await fs.writeFile(
+      `public${imagePath}`,
+      Buffer.from(await data.image.arrayBuffer())
+    )
+  }
 
-  await fs.mkdir("public/products", { recursive: true })
-  const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
-  await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
-
-  await db.product.create({ 
-    data: { 
-      isAvailableForPurchase: false,
+  await db.product.update({
+    where: { id },
+    data: {
       name: data.name,
       description: data.description,
       priceInCents: data.priceInCents,
@@ -106,7 +112,7 @@ if (data.image != null && data.image.size > 0) {
 export async function toggleProductAvailability(
   id: string,
   isAvailableForPurchase: boolean
-  ) {
+) {
   await db.product.update({ where: { id }, data: { isAvailableForPurchase } })
 
   revalidatePath("/")
@@ -114,10 +120,13 @@ export async function toggleProductAvailability(
 }
 
 export async function deleteProduct(id: string) {
-  const product = await db.product.delete({ where: { id }})
+  const product = await db.product.delete({ where: { id } })
 
   if (product == null) return notFound()
 
   await fs.unlink(product.filePath)
   await fs.unlink(`public${product.imagePath}`)
+
+  revalidatePath("/")
+  revalidatePath("/products")
 }
--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -6,36 +6,61 @@ import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { formatCurrency } from "@/lib/formatters"
 import { useState } from "react"
-import { addProduct } from "../../_actions/products"
+import { addProduct, updateProduct } from "../../_actions/products"
 import { useFormState, useFormStatus } from "react-dom"
 import { Product } from "@prisma/client"
 import Image from "next/image"
 
-export function ProductForm({ product }:{ product?: Product | null }) {
-  const [error, action] = useFormState(addProduct, {})
-  const [priceInCents, setPriceInCents] = useState<number | undefined>(product?.priceInCents)
-
+export function ProductForm({ product }: { product?: Product | null }) {
+  const [error, action] = useFormState(
+    product == null ? addProduct : updateProduct.bind(null, product.id),
+    {}
+  )
+  const [priceInCents, setPriceInCents] = useState<number | undefined>(
+    product?.priceInCents
+  )
 
   return (
     <form action={action} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required defaultValue={product?.name || ""} />
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          required
+          defaultValue={product?.name || ""}
+        />
         {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="priceInCents">Price In Cents</Label>
-        <Input type="text" id="priceInCents" name="priceInCents" required value={priceInCents} onChange={e => setPriceInCents(Number(e.target.value) || undefined)}
+        <Input
+          type="number"
+          id="priceInCents"
+          name="priceInCents"
+          required
+          value={priceInCents}
+          onChange={e => setPriceInCents(Number(e.target.value) || undefined)}
         />
         <div className="text-muted-foreground">
           {formatCurrency((priceInCents || 0) / 100)}
-          {error.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
         </div>
+        {error.priceInCents && (
+          <div className="text-destructive">{error.priceInCents}</div>
+        )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Textarea id="description" name="description" required defaultValue={product?.description || ""} />
-        {error.description && <div className="text-destructive">{error.description}</div>}
+        <Textarea
+          id="description"
+          name="description"
+          required
+          defaultValue={product?.description}
+        />
+        {error.description && (
+          <div className="text-destructive">{error.description}</div>
+        )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
@@ -47,26 +72,28 @@ export function ProductForm({ product }:{ product?: Product | null }) {
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
+        <Input type="file" id="image" name="image" required={product == null} />
         {product != null && (
-          <Image 
-            src={"product.imagePath"} 
-            height="400" 
-            width="400" 
-            alt={"Product Image"}
+          <Image
+            src={product.imagePath}
+            height="400"
+            width="400"
+            alt="Product Image"
           />
         )}
         {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
       <SubmitButton />
     </form>
-    )
-  }
+  )
+}
 
-  function SubmitButton() {
-    const { pending } = useFormStatus()
-    return ( 
-    <Button type="submit"disabled={pending}>
+function SubmitButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <Button type="submit" disabled={pending}>
       {pending ? "Saving..." : "Save"}
     </Button>
-    )
-  }
+  )
+}
