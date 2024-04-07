23841689e342e0ad098189b3c1d3e12ac1d2b078
kevin <kthurman59@gmail.com>
2024-04-07 13:52:28 -0500

    schema update

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -51,17 +51,36 @@ export async function addProduct(prevState: unknown, formData: FormData) {
   redirect("/admin/products")
 }
 
+const editSchema = addSchema.extend({
+  file: fileSchema.optional(),
+  image: imageSchema.optional()
+})
+
 export async function updateProduct(id: string, prevState: unknown, formData: FormData) {
-  const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
+  const result = editSchema.safeParse(Object.fromEntries(formData.entries()))
   if (result.success === false) {
     return result.error.formErrors.fieldErrors
   }
 
   const data = result.data
+  const product = await db.product.findUnique({ where: {
+    id } })
 
-  await fs.mkdir("products", { recursive: true })
-  const filePath = `products/${crypto.randomUUID()}-${data.file.name}`
-  await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+  if (product == null) return notFound()
+
+  let filePath = product.filePath
+  if (data.file != null && data.file.size > 0) {
+    await fs.unlink(product.filePath)
+    filePath = `products/${crypto.randomUUID()}-${data.file.name}`
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+}
+
+let imagePath = product.imagePath
+if (data.image != null && data.image.size > 0) {
+  await fs.unlink(`public${product.imagePath}`)
+  imagePath = `products/${crypto.randomUUID()}-${data.image.name}`
+  await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+}
 
   await fs.mkdir("public/products", { recursive: true })
   const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
