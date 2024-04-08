663bf628d1f6b128b5f7af0ef692dfb193e741fb
kevin <kthurman59@gmail.com>
2024-04-08 08:18:11 -0500

    route

--- src/app/admin/products/[id]/download/route.ts
+++ src/app/admin/products/[id]/download/route.ts
@@ -1,5 +1,7 @@
 import db from "@/db/db";
-import { NextRequest } from "next/server";
+import { notFound } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs/promises"
 
 export async function GET(req: NextRequest, { params: { id }}: { params: { id: string } }  
 ) {
@@ -7,4 +9,16 @@ export async function GET(req: NextRequest, { params: { id }}: { params: { id: s
     where: { id },
     select: { filePath: true, name: true },
   })
+
+  if (product == null) return notFound()
+
+  const { size } = await fs.stat(product.filePath)
+  const file = await fs.readFile(product.filePath)
+  const extension = product.filePath.split(".").pop()
+
+  return new NextResponse(file, { headers: {
+    "Content-Disposition": `attachment; filename="${product.name}.${extension}"`,
+    "Content-Length": size.toString(),
+  },
+ })
 }
