170fd4c5a396279994206e2f28e5187b22e3b183
kevin <kthurman59@gmail.com>
2024-04-08 08:04:13 -0500

    update

--- src/app/admin/products/[id]/download/route.ts
+++ src/app/admin/products/[id]/download/route.ts
@@ -1,8 +1,9 @@
 import db from "@/db/db";
 import { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest, { params: { id }}: { params: { id: string }}) {
-  const data = await db.product.findUnique({
+export async function GET(req: NextRequest, { params: { id }}: { params: { id: string } }  
+) {
+  const product = await db.product.findUnique({
     where: { id },
     select: { filePath: true, name: true },
   })
