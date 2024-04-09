3cb4888b891f5d7006518230eaa1158c0c399c73
kevin <kthurman59@gmail.com>
2024-04-09 11:47:52 -0500

    product set up

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
 import db from "@/db/db"
+import { Product } from "@prisma/client"
+import { ArrowRight } from "lucide-react"
+import Link from "next/link"
 
-
-function getNewestProducts() {
+function getMostPopularProducts() {
   return db.product.findMany({
     where: { isAvailableForPurchase: true },
     orderBy: { orders: { _count: "desc" } },
@@ -10,6 +13,43 @@ function getNewestProducts() {
 }
 
 
+function getNewestProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { createdAt: "desc" },
+    take: 6
+  })
+}
+
+
 export default function HomePage() {
-  return <h1>HELLO!</h1>
+  return <main className="space-y-12">
+    <ProductGridSection title="Most Popular" productsFetcher={getMostPopularProducts} />
+    <ProductGridSection title="Newest" productsFetcher={getNewestProducts} />
+  </main>
+}
+
+type ProductsGridSectionProps = {
+  title: string
+  productsFetcher: () => Promise<Product[]>
+}
+
+function ProductGridSection({ productsFetcher, title}:
+  ProductsGridSectionProps) {
+ return (
+  <div className="space-y-4">
+    <div className="flex gap-4">
+      <h2 className="text-3xl font-bold">{title}</h2>
+      <Button variant="outline" asChild>
+        <Link href="/products" className="space-x-2">
+          <span>View All</span>
+          <ArrowRight className="size-4" />
+        </Link>
+      </Button>
+    </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <ProductCard />
+    </div>
+  </div>
+ )
 }
