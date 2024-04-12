255997581563b3fff91f26b9c6404cf8606da830
kevin <kthurman59@gmail.com>
2024-04-12 05:00:35 -0500

    cache init

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -1,14 +1,18 @@
 import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard"
 import db from "@/db/db"
+import { cache } from "@/lib/cache"
 import { Suspense } from "react"
 
-function getProducts() {
-  return db.product.findMany({ where: {
-    isAvailableForPurchase: true
-  } })
-}
+const getProducts = cache(() => {
+  return db.product.findMany({ 
+    where: { isAvailableForPurchase: true },
+    orderBy: { name: "asc" }, 
+  })
+}, ["/products", "getProducts"]
+)
 
 export default function ProductsPage() {
+  return (
   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <Suspense fallback={
         <>
@@ -24,6 +28,7 @@ export default function ProductsPage() {
         <ProductsSuspense />
       </Suspense>
     </div>
+  )
 }
 
 async function ProductsSuspense() {
