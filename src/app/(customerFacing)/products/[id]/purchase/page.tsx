53967660961aaa270d7b07348d443ad9f39813c9
kevin <kthurman59@gmail.com>
2024-04-12 09:31:19 -0500

    page

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,5 +1,8 @@
 import db from "@/db/db"
 import { notFound } from "next/navigation"
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export default async function PurchasePage({
   params: { id },
@@ -11,5 +14,15 @@ export default async function PurchasePage({
   })
   if (product == null) return notFound()
 
-  return <h1>Hello!</h1>
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: "USD",
+    metadata: { productId: product.id }
+  })
+
+  if (paymentIntent.client_secret == null) {
+    throw Error("Stripe failed to create payment intent")
+  }
+
+  return <CheckoutForm product={product} clientSecret={paymentIntent.client_secret} />
 }
