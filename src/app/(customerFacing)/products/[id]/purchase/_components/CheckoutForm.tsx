7f06743ce263ce9457f9cb6981943b903193cbab
kevin <kthurman59@gmail.com>
2024-04-14 07:30:09 -0500

    updates

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { userOrderExists } from "@/app/actions/orders"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { formatCurrency } from "@/lib/formatters"
@@ -8,9 +9,9 @@ import { loadStripe } from "@stripe/stripe-js"
 import Image from "next/image"
 import { FormEvent, useState } from "react"
 
-
 type CheckoutFormProps = {
   product: {
+    id: string
     imagePath: string
     name: string
     priceInCents: number
@@ -37,27 +38,35 @@ export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
    </div>
   </div>
   <Elements options={{ clientSecret }} stripe={stripePromise}>
-    <Form priceInCents={product.priceInCents} />
+    <Form priceInCents={product.priceInCents} productId={product.id} />
   </Elements>
   </div>
   )
 }
 
-function Form({ priceInCents }: { priceInCents: number}) {
+function Form({ priceInCents, productId }: { priceInCents: number, productId: string }) {
   const stripe = useStripe()
   const elements = useElements()
   const [isLoading, setIsLoading] = useState(false)
   const [errorMessage, setErrorMessage] = useState<string>()
+  const [email, setEmail] = useState<string>()
 
-
-  function handleSubmit(e: FormEvent) {
+  async function handleSubmit(e: FormEvent) {
     e.preventDefault()
 
-    if (stripe == null || elements == null || isLoading) return
+    if (stripe == null || elements == null || email === null) return
 
     setIsLoading(true)
 
-    // Check for existing order (don't want customer placing same order twice for free)
+    const orderExists = await userOrderExists(email, productId)
+
+    if (orderExists) {
+      setErrorMessage(
+        "You have already purchased this product. Try downloading it from the My Orders page"
+      )
+      setIsLoading(false)
+      return
+    }
 
     stripe
       .confirmPayment({
@@ -92,7 +101,7 @@ function Form({ priceInCents }: { priceInCents: number}) {
       <CardContent>
         <PaymentElement />
         <div className="mt-4">
-          <LinkAuthenticationElement />
+          <LinkAuthenticationElement onChange={e => setEmail(e.value.email)} />
         </div>
       </CardContent>
         <CardFooter>
