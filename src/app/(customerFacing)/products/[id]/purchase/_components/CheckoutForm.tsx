fe090b4574fb2afc0b730e873ea602f265e674a1
kevin <kthurman59@gmail.com>
2024-04-14 02:06:47 -0500

    stripe logic

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,8 +1,22 @@
+import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
+
+
 type CheckoutFormProps = {
   product: {}
   clientSecret: string
 }
 
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string)
+
 export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
-  return <h1>Form</h1>
+  return <Elements options={{ clientSecret }} stripe={stripePromise}>
+    <Form />
+  </Elements>
+}
+
+function Form() {
+  const stripe = useStripe()
+  const elements = useElements()
+  return <PaymentElement />
 }
