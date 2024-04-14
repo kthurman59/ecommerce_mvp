4212ece4562509481ae7a4058275d7e99b534286
kevin <kthurman59@gmail.com>
2024-04-14 02:52:19 -0500

    new page

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,9 +1,12 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { formatCurrency } from "@/lib/formatters"
 import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
 import Image from "next/image"
+import { FormEvent, useState } from "react"
 
 
 type CheckoutFormProps = {
@@ -24,25 +27,65 @@ export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
   <div className="max-w-5xl w-full mx-auto space-y-8">
     <div className="flex gap-4 items-center">
   <div className="aspect-video flex-shrink-0 w-1/3 relative">
-    <Image src={product.imagePath} fill alt={product.name} />
+    <Image src={product.imagePath} fill alt={product.name} className="object-cover" />
   </div>
     <div>
       <div className="text-lg">
         {formatCurrency(product.priceInCents / 100)}
       </div>
       <h1 className="text-2xl font-bold">{product.name}</h1>
+      <div className="line-clamp-3 text-muted-foreground">{product.description}</div>
    </div>
   </div>
   <Elements options={{ clientSecret }} stripe={stripePromise}>
-    <Form />
+    <Form priceInCents={product.priceInCents} />
   </Elements>
   </div>
   </>
   )
 }
 
-function Form() {
+function Form({ priceInCents }: { priceInCents: number}) {
   const stripe = useStripe()
   const elements = useElements()
-  return <PaymentElement />
+  const [isLoading, setIsLoading] = useState(false)
+
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault()
+
+    if (stripe == null || elements == null || isLoading) return
+
+    setIsLoading(true)
+
+    // Check for existing order (don't want customer placing same order twice for free)
+
+    stripe.confirmPayment({ elements, confirmParams: {
+      return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase-success`
+    }})
+  }
+
+  return (
+  <form onSubmit={handleSubmit}>
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          Checkout
+        </CardTitle>
+        <CardDescription className="text-destructive">Error</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <PaymentElement />
+      </CardContent>
+        <CardFooter>
+          <Button className="w-full" size="lg" disabled={stripe == null || elements == null || isLoading}
+        >
+          {isLoading 
+            ? "Purchasing..." 
+            : `Purchase - ${formatCurrency(priceInCents / 100)}`}
+          </Button>
+        </CardFooter>
+    </Card>
+  </form>
+  )
 }
