514a4e21094e8d8cfb562f7f67fd9c871256ef4d
kevin <kthurman59@gmail.com>
2024-04-22 10:31:11 -0500

    orderactions

--- src/app/(customerFacing)/products/download/[downloadVerification]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerification]/route.ts
@@ -1,7 +1,6 @@
-import db from "@/db/db";
-import { NextRequest, NextResponse } from "next/server";
-import fs from "fs/promises";
-
+import db from "@/db/db"
+import { NextRequest, NextResponse } from "next/server"
+import fs from "fs/promises"
 
 export async function GET(
   req: NextRequest,
@@ -11,19 +10,21 @@ export async function GET(
 ) {
   const data = await db.downloadVerification.findUnique({
     where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
-    select: { product: { select: { filePath: true, name: true }}}
+    select: { product: { select: { filePath: true, name: true } } },
   })
 
   if (data == null) {
     return NextResponse.redirect(new URL("/products/download/expired", req.url))
   }
+
   const { size } = await fs.stat(data.product.filePath)
   const file = await fs.readFile(data.product.filePath)
   const extension = data.product.filePath.split(".").pop()
 
-  return new NextResponse(file, { headers: {
-    "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
-    "Content-Length": size.toString(),
-  },
- })
+  return new NextResponse(file, {
+    headers: {
+      "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+      "Content-Length": size.toString(),
+    },
+  })
 }
