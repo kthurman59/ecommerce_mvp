d032d2aa18ea9509f112f38438623fb9401a7e25
kevin <kthurman59@gmail.com>
2024-04-25 10:06:26 -0500

    receipts and emails

---
+++ src/email/components/OrderInformation.tsx
@@ -0,0 +1,12 @@
+type OrderinformationProps = {
+  order: {}
+  product: {}
+  downloadVerificationId: string
+}
+
+
+export function OrderInformation ({ order, product, downloadVerificationId }: OrderinformationProps) {
+  return (
+    <h1>HI</h1>
+  )
+}
